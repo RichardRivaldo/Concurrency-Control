cd633ad652278282f75846399079a3aa62913ca8
RichardRivaldo <richardrivaldo84@gmail.com>
2021-11-27 18:22:31 +0700

    divide operation into w/r, refactor transaction

--- operations/operations.py
+++ operations/operations.py
@@ -1,28 +1,41 @@
 # Operation implementation
-from dataclasses import dataclass
 
 from data.data import Data
 from utils.operation_type import OperationType
 
 
-@dataclass
 class Operation:
-    transaction: int
-    operation_type: OperationType
-    target_data: Data
+    def __init__(self, transaction: int, operation_type: OperationType, target_data: Data):
+        self.transaction: int = transaction
+        self.operation_type: OperationType = operation_type
+        self.target_data: Data = target_data
 
     def log(self, message: str):
         print(f"{self} {message}")
 
-    def execute_read(self):
-        self.log(f"{self.target_data}")
-
-    def execute_write(self, new_value: int):
-        self.target_data.set_value(new_value=new_value)
-        self.log(f"Successfully wrote the data!")
+    def execute(self):
+        pass
 
     def get_target_data(self):
         return self.target_data
 
     def __repr__(self) -> str:
         return f"[T{self.transaction}: {self.operation_type.value} {self.target_data.get_name()}]"
+
+
+class ReadOperation(Operation):
+    def __init__(self, transaction: int, target_data: Data):
+        Operation.__init__(self, transaction=transaction, operation_type=OperationType.READ, target_data=target_data)
+
+    def execute(self):
+        self.log(f"{self.target_data}")
+
+
+class WriteOperation(Operation):
+    def __init__(self, transaction: int, target_data: Data, new_value: int):
+        Operation.__init__(self, transaction=transaction, operation_type=OperationType.WRITE, target_data=target_data)
+        self.new_value: int = new_value
+
+    def execute(self):
+        self.target_data.set_value(new_value=self.new_value)
+        self.log(f"Successfully wrote the value of the data to {self.new_value}!")
--- transaction/transaction.py
+++ transaction/transaction.py
@@ -4,29 +4,37 @@ from dataclasses import dataclass
 from typing import List
 
 from operations.operations import Operation
+from utils.transaction_status import TransactionStatus
 
 
 @dataclass
 class Transaction:
-    transaction: int
+    transaction_id: int
     operations: List[Operation]
-    current_operation: int
+    current_operation: int = 0
+    status: TransactionStatus = TransactionStatus.INCOMPLETE
+
+    def __repr__(self) -> str:
+        return f"[T{self.transaction_id}]"
+
+    def log(self, message: str):
+        print(f"{self} {message}")
+
+    def set_status(self, new_status: TransactionStatus):
+        self.status = new_status
 
     def increment_operation_idx(self):
         self.current_operation += 1
 
-    def execute_read(self):
-        self.operations[0].execute_read()
-        self.increment_operation_idx()
-
-    def execute_write(self):
-        self.operations[0].execute_write()
-        self.increment_operation_idx()
-
-    def execute_commit(self):
-        self.log("The transaction commited successfully!")
+    def execute(self):
+        self.operations[self.current_operation].execute()
         self.increment_operation_idx()
 
     def execute_abort(self):
-        self.log("The transaction is aborted!")
         self.current_operation = 0
+        self.set_status(TransactionStatus.ABORTED)
+        self.log("The transaction committed successfully!")
+
+    def execute_commit(self):
+        self.set_status(TransactionStatus.COMMITTED)
+        self.log("The transaction is aborted!")
