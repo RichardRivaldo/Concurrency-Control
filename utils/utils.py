805f2d27df109ebec2f49148defa4894996f07c6
RichardRivaldo <richardrivaldo84@gmail.com>
2021-11-28 16:07:23 +0700

    modified utils and added dummy schedules

--- utils/utils.py
+++ utils/utils.py
@@ -1,23 +1,24 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
 from data.data import Data
-from operations.operations import Operation, WriteOperation, ReadOperation
+from operations.operations import Operation, WriteOperation, ReadOperation, CommitOperation
 from transaction.transaction import Transaction
 
 
-def create_transactions(operations: List[Operation]) -> List[Transaction]:
-    result: List[Transaction] = []
-    transactions = dict()
+def create_transactions(operations: List[Operation]) -> Dict[int, Transaction]:
+    ops_trans: Dict[int, List[Operation]] = dict()
+    transactions: Dict[int, Transaction] = dict()
     for operation in operations:
-        if operation.transaction not in transactions.keys():
-            transactions[operation.transaction] = [operation]
+        transaction_id = operation.get_transaction()
+        if transaction_id not in ops_trans.keys():
+            ops_trans[transaction_id] = [operation]
         else:
-            transactions[operation.transaction].extend([operation])
+            ops_trans[transaction_id].extend([operation])
 
-    for transaction in transactions.keys():
-        result.append(Transaction(transaction_id=transaction, operations=transactions[transaction]))
+    for transaction in ops_trans.keys():
+        transactions[transaction] = Transaction(transaction_id=transaction, operations=ops_trans.get(transaction))
 
-    return result
+    return transactions
 
 
 def create_dummy_schedule() -> Tuple[List[Data], List[Operation]]:
@@ -25,12 +26,16 @@ def create_dummy_schedule() -> Tuple[List[Data], List[Operation]]:
     data_b = Data(data_name="B", data_value=2)
     data_c = Data(data_name="C", data_value=3)
 
-    op_1 = WriteOperation(transaction=1, target_data=data_a, new_value=5)
-    op_2 = ReadOperation(transaction=2, target_data=data_b)
-    op_3 = WriteOperation(transaction=1, target_data=data_c, new_value=2)
+    # op_0 = ReadOperation(transaction=1, target_data=data_a)
+    op_1 = WriteOperation(transaction=3, target_data=data_a, new_value=5)
+    op_2 = ReadOperation(transaction=2, target_data=data_c)
+    op_3 = WriteOperation(transaction=1, target_data=data_a, new_value=2)
     op_4 = WriteOperation(transaction=3, target_data=data_c, new_value=765)
-    op_5 = ReadOperation(transaction=2, target_data=data_b)
+    op_5 = ReadOperation(transaction=2, target_data=data_a)
+    op_6 = CommitOperation(transaction=2)
+    op_7 = CommitOperation(transaction=1)
+    op_8 = CommitOperation(transaction=3)
 
     data = [data_a, data_b, data_c]
-    ops = [op_1, op_2, op_3, op_4, op_5]
+    ops = [op_1, op_2, op_3, op_4, op_5, op_6, op_7, op_8]
     return data, ops
